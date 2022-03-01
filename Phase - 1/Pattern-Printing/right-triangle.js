665fd807baa9f7c5239772f1de84b0a391c78d3b
rksdev <mailto.dev.rakesh@gmail.com>
2022-03-01 11:13:39 +0530

    right triangle completed

--- Phase - 1/Pattern-Printing/right-triangle.js
+++ Phase - 1/Pattern-Printing/right-triangle.js
@@ -1,3 +1,16 @@
 function rightTriangle(side) {
   let starString = "";
+  for (let i = 1; i <= side; i++) {
+    for (let j = 1; j <= side; j++) {
+      if (j >= side - i + 1) {
+        starString += "*";
+      } else {
+        starString += " ";
+      }
+    }
+    starString += "\n";
+  }
+  console.log(starString);
 }
+
+rightTriangle(5);
