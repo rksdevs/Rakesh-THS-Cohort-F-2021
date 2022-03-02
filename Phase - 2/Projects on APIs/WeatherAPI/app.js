11431f7e3875a130a847743dc946bb184de5f534
rksdev <mailto.dev.rakesh@gmail.com>
2022-03-02 21:32:43 +0530

    Weather API Complete

---
+++ Phase - 2/Projects on APIs/WeatherAPI/app.js
@@ -0,0 +1,80 @@
+// import readline from "readline-sync";
+// import axios from "axios";
+// import fs from "fs";
+
+const fs = require("fs");
+const readline = require("readline-sync");
+const axios = require("axios");
+
+let input = readline.question("Enter the name of the city: ");
+let weatherData = "";
+let newWeatherObj = "";
+let now = Date.now();
+
+const read = (fileName) => {
+  return new Promise((res, rej) => {
+    fs.readFile(fileName, "utf-8", (data, err) => {
+      if (err) {
+        return rej(err);
+      } else {
+        // res(data);
+      }
+    });
+  });
+};
+
+const write = (fileName, dataToWrite) => {
+  return new Promise((res, rej) => {
+    fs.writeFile(fileName, dataToWrite, (err) => {
+      if (err) {
+        rej(err);
+      } else {
+        // res(`Writing into ${fileName} successful.`);
+        res(dataToWrite);
+      }
+    });
+  });
+};
+
+const apndFile = (dataToAppend) => {
+  return new Promise((res, rej) => {
+    fs.appendFile("city.txt", dataToAppend, (err) => {
+      if (err) {
+        rej(err);
+      }
+    });
+  });
+};
+
+axios
+  .get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=6db7b86c779bdc1e4883492d7a81f4a9&units=metric`
+  )
+  .then((res) => {
+    return write("data.json", JSON.stringify(res.data));
+  })
+  .then((data) => {
+    console.log("writing success");
+    return data;
+  })
+  .then((data) => {
+    let weatherDetails = JSON.parse(data);
+    let newWeatherObj = {
+      name: weatherDetails.name,
+      weather: weatherDetails.weather,
+      main: weatherDetails.main,
+    };
+    return write(
+      `${newWeatherObj.name}-${now}.json`,
+      JSON.stringify(newWeatherObj)
+    );
+  })
+  .then((data) => {
+    apndFile(data);
+  })
+  .then(() => {
+    console.log("task completed");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
