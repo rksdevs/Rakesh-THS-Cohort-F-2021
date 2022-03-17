ec64bd0c095a0687911f2dfee0ff04147abd8210
rksdev <mailto.dev.rakesh@gmail.com>
2022-03-17 19:41:40 +0530

    JWT & Login User Completed - API

--- Phase - 3/learners-inn/Backend/controllers/authController.js
+++ Phase - 3/learners-inn/Backend/controllers/authController.js
@@ -37,10 +37,39 @@ const registerUser = async (req, res) => {
     res.status(200).json({ info: "User registered", user });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ info: error });
+    return res.status(500).json({ info: "User registration failed", error });
   }
 };
 
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.status(200).json({
+        info: "Login successful",
+        user: user.userName,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      return res.status(400).json({ info: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ info: "Login user failed", error });
+  }
+};
+
+//Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET_DEV, {
+    expiresIn: "1d",
+  });
+};
 module.exports = {
   registerUser,
+  loginUser,
 };
--- Phase - 3/learners-inn/Backend/routes/authRoutes.js
+++ Phase - 3/learners-inn/Backend/routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/authController");
+const { registerUser, loginUser } = require("../controllers/authController");
 
 //Register user
-// router.get("/", (req, res) => {
-//   res.send("Hey its auth route");
-// });
 router.post("/register", registerUser);
+
+//Login User
+router.post("/login", loginUser);
 
 module.exports = router;
