49ce2efb28d98ee5472696525d62727d3f1cf867
rksdev <mailto.dev.rakesh@gmail.com>
2022-03-23 22:58:27 +0530

    Route Setup Completed

--- Phase - 3/learners-inn/client/src/App.js
+++ Phase - 3/learners-inn/client/src/App.js
@@ -2,13 +2,37 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Home /> {/* <Profile /> */}
-      {/* <Login /> */}
-      {/* <Register /> */}
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {/* {user ? <Home /> : <Register />} */}
+          <Home />
+        </Route>
+        <Route path="/register">
+          {/* {user ? <Home /> : <Register />} */}
+          <Register />
+        </Route>
+        <Route path="/login">
+          {/* {user ? <Home /> : <Register />} */}
+          <Login />
+        </Route>
+        {/* <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route> */}
+        {/* <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route> */}
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
