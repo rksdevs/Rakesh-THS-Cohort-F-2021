0275549894bf5a21da2fd24e6f0966b53d12ef30
rksdev <mailto.dev.rakesh@gmail.com>
2022-03-23 12:58:30 +0530

    homepage, feed, rightbar and sidebar complete

--- Phase - 3/learners-inn/client/src/pages/home/Home.jsx
+++ Phase - 3/learners-inn/client/src/pages/home/Home.jsx
@@ -1,6 +1,20 @@
 import Topbar from "../../components/Topbar";
+import Feed from "../../components/feed/Feed";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./home.css";
+
 const home = () => {
-  return <Topbar />;
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
 };
 
 export default home;
