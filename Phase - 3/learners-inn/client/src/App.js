cfcd064671df176e785b4314c5aba538f5ca0219
rksdev <mailto.dev.rakesh@gmail.com>
2022-03-23 13:27:07 +0530

    Login, Register, Profile Page Design Complete

--- Phase - 3/learners-inn/client/src/App.js
+++ Phase - 3/learners-inn/client/src/App.js
@@ -1,8 +1,13 @@
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
 function App() {
   return (
     <div>
-      <Home />{" "}
+      <Home /> {/* <Profile /> */}
+      {/* <Login /> */}
+      {/* <Register /> */}
     </div>
   );
 }
--- Phase - 3/learners-inn/client/src/pages/home/Home.jsx
+++ Phase - 3/learners-inn/client/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import Topbar from "../../components/Topbar";
+import Topbar from "../../components/topbar/Topbar";
 import Feed from "../../components/feed/Feed";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Rightbar from "../../components/rightbar/Rightbar";
