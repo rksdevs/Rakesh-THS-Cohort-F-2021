5c510a1c69943c13a27a1b41fd896aa414b434b1
rksdev <mailto.dev.rakesh@gmail.com>
2022-03-31 23:49:28 +0530

    updated remove duplicates

--- Phase - 1/DSA/remove-duplicates.js
+++ Phase - 1/DSA/remove-duplicates.js
@@ -5,4 +5,21 @@ Sample Output : [1, 2, 3, 4, 5]
 Number of Elements Removed : 4
 */
 
-let arr = [1, 2, 2, 3, 4, 4, 4, 5, 5];
+// let arr = [1, 2, 2, 3, 4, 4, 4, 5, 5];
+
+let removedElem = 0;
+
+function removeDupElem(arr1) {
+  for (let i = 0; i <= arr1.length - 1; i++) {
+    for (let j = 0; j <= arr1.length; j++) {
+      if (arr1[i] === arr1[j] && i !== j) {
+        arr1.splice(arr1[j], 1);
+        removedElem++;
+      }
+    }
+  }
+  console.log(arr1);
+  console.log(removedElem);
+}
+
+removeDupElem([1, 2, 2, 3, 4, 4, 4, 5, 5]);
