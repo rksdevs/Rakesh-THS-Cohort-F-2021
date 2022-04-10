a5eab9cdeef82ddda8301ffccf8adf0590ef4880
rksdev <mailto.dev.rakesh@gmail.com>
2022-04-10 21:15:16 +0530

    reverse String

---
+++ Phase - 1/DSA/reverseString.js
@@ -0,0 +1,37 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// function reverse(str) {
+//   let result = [];
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     result.push(str[i]);
+//   }
+
+//   return result.join("");
+// }
+
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
+
+function reverse(str) {
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
+
+console.log(reverse("apple"));
