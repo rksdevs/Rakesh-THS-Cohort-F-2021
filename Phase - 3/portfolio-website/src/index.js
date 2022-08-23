64c19baa56801369c77b7a4d559ff74e253dd3d2
rksdev <mailto.dev.rakesh@gmail.com>
2022-08-23 22:12:49 +0530

    updated portfolio components

--- Phase - 3/portfolio-website/src/index.js
+++ Phase - 3/portfolio-website/src/index.js
@@ -1,10 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import GlobalStyle from './styles/GlobalStyle';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
