2a183b698f56a8faef473dd7fb66a927988c3d46
rksdev <mailto.dev.rakesh@gmail.com>
2022-08-23 22:48:42 +0530

    Home page completed

--- Phase - 3/portfolio-website/src/Components/ImageSection.js
+++ Phase - 3/portfolio-website/src/Components/ImageSection.js
@@ -1,90 +1,94 @@
-import React from 'react'
-import styled from 'styled-components';
-import resume from '../img/resume.jpg';
-import PrimaryButton from './PrimaryButton';
+import React from "react";
+import styled from "styled-components";
+import resume from "../img/Profile-pic2.jpg";
+import PrimaryButton from "./PrimaryButton";
 
 function ImageSection() {
-    return (
-        <ImageSectionStyled>
-            <div className="left-content">
-                <img src={resume} alt=""/>
-            </div>
-            <div className="right-content">
-                <h4>I am <span>Lorem Ipsum</span></h4>
-                <p className="paragraph">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, natus quas vero enim 
-                    praesentium delectus est id fugiat ab libero adipisci recusandae at maxime veritatis! 
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, nesciunt.
-                    
-                </p>
-                <div className="about-info">
-                    <div className="info-title">
-                        <p>Full Name</p>
-                        <p>Age</p>
-                        <p>Nationality </p>
-                        <p>Languages </p>
-                        <p>Location</p>
-                        <p>Service</p>
-                    </div>
-                    <div className="info">
-                        <p>: Lorem Ipsum</p>
-                        <p>: 36</p>
-                        <p>: Spainsh </p>
-                        <p>: Spanish, French, English </p>
-                        <p>: London, United Kingdom</p>
-                        <p>: Freelance</p>
-                    </div>
-                </div>
-                <PrimaryButton title={'Download Cv'} />
-            </div>
-        </ImageSectionStyled>
-    )
+  return (
+    <ImageSectionStyled>
+      <div className="left-content">
+        <img src={resume} alt="" />
+      </div>
+      <div className="right-content">
+        <h4>
+          I am <span>Rakesh Sahu</span>
+        </h4>
+        <p className="paragraph">
+          I'm full-stack developer graduated from The Hacking School, Hyderabad
+          web development bootcamp in 2022. I specialize in MERN stack, and
+          comfortable in working in any related roles. Perosnally, I'm highly
+          motivated and have a keen interest in development. While I'm currently
+          working as an Assistant Manager in a reputed MNC in backend process,
+          I'm actively looking jobs in IT domain.
+        </p>
+        <div className="about-info">
+          <div className="info-title">
+            <p>Full Name</p>
+            <p>Age</p>
+            <p>Nationality </p>
+            <p>Languages </p>
+            <p>Location</p>
+            <p>Service</p>
+          </div>
+          <div className="info">
+            <p>: Lorem Ipsum</p>
+            <p>: 36</p>
+            <p>: Spainsh </p>
+            <p>: Spanish, French, English </p>
+            <p>: London, United Kingdom</p>
+            <p>: Freelance</p>
+          </div>
+        </div>
+        <PrimaryButton title={"Download Cv"} />
+      </div>
+    </ImageSectionStyled>
+  );
 }
 
-
 const ImageSectionStyled = styled.div`
-    margin-top: 5rem;
-    display: flex;
-    @media screen and (max-width:1000px){
-        flex-direction: column;
-        .left-content{
-            margin-bottom: 2rem;
-        }
+  margin-top: 5rem;
+  display: flex;
+  @media screen and (max-width: 1000px) {
+    flex-direction: column;
+    .left-content {
+      margin-bottom: 2rem;
     }
-    .left-content{
-        width: 100%;
-        img{
-            width: 95%;
-            object-fit: cover;
-        }
+  }
+  .left-content {
+    width: 100%;
+    img {
+      width: 95%;
+      object-fit: cover;
     }
-    .right-content{
-        width: 100%;
-        h4{
-            font-size: 2rem;
-            color: var(--white-color);
-            span{
-                font-size: 2rem;
-            }
-        }
-        .paragraph{
-            padding: 1rem 0;
-        }
-        .about-info{
-            display: flex;
-            padding-bottom: 1.4rem;
-            .info-title{
-                padding-right: 3rem;
-                p{
-                    font-weight: 600;
-                }
-            }
-            .info-title, .info{
-                p{
-                    padding: .3rem 0;
-                }
-            }
-        }
+  }
+  .right-content {
+    width: 100%;
+    h4 {
+      font-size: 2rem;
+      color: var(--white-color);
+      span {
+        font-size: 2rem;
+      }
     }
+    .paragraph {
+      padding: 1rem 0;
+    }
+    .about-info {
+      display: flex;
+      padding-bottom: 1.4rem;
+      .info-title {
+        padding-right: 3rem;
+        p {
+          font-weight: 600;
+        }
+      }
+      .info-title,
+      .info {
+        p {
+          padding: 0.3rem 0;
+        }
+      }
+    }
+  }
 `;
 export default ImageSection;
