ca4c39a997f5c5ab5a05edc3fd3d6315e1cc8500
rksdev <mailto.dev.rakesh@gmail.com>
2022-08-24 00:19:06 +0530

    Resume section completed

--- Phase - 3/portfolio-website/src/Components/Resume.js
+++ Phase - 3/portfolio-website/src/Components/Resume.js
@@ -1,80 +1,90 @@
-import React from 'react';
-import styled from 'styled-components';
-import {InnerLayout} from '../styles/Layouts';
-import Title from '../Components/Title';
-import SmallTitle from '../Components/SmallTitle';
-import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
-import SchoolIcon from '@material-ui/icons/School';
-import ResumeItem from '../Components/ResumeItem';
+import React from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../styles/Layouts";
+import Title from "../Components/Title";
+import SmallTitle from "../Components/SmallTitle";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import SchoolIcon from "@material-ui/icons/School";
+import ResumeItem from "../Components/ResumeItem";
 
 function Resume() {
-    const briefcase = <BusinessCenterIcon />
-    const school = <SchoolIcon />
-    return (
-        <ResumeStyled>
-            <Title title={'Resume'} span={'resume'} />
-            <InnerLayout>
-                <div className="small-title">
-                    <SmallTitle icon={briefcase} title={'Working Experience'} />
-                </div>
-                <div className="resume-content">
-                    <ResumeItem 
-                        year={'2015 - 2020'} 
-                        title={'Computer Science Teacher'}
-                        subTitle={'Sussex University'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
-                    />
-                    <ResumeItem 
-                        year={'2015 - Present'} 
-                        title={'Full Stack Developer'}
-                        subTitle={'Microsoft Studios'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
-                    />
-                    <ResumeItem 
-                        year={'2010 - 2017'} 
-                        title={'User Interface Designer'}
-                        subTitle={'Google Inc'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
-                    />
-                </div>
-                <div className="small-title u-small-title-margin">
-                    <SmallTitle icon={school} title={'Educational Qualifications'} />
-                </div>
-                <div className="resume-content ">
-                    <ResumeItem 
-                        year={'201 - 2022'} 
-                        title={'Computer Science Degree'}
-                        subTitle={'Sussex University'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
-                    />
-                    <ResumeItem 
-                        year={'2015 - 2017'} 
-                        title={'A - Levels'}
-                        subTitle={'Church Hill High'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
-                    />
-                    <ResumeItem 
-                        year={'2015 - 2017'} 
-                        title={'High School Graduation'}
-                        subTitle={'ABC School'}
-                        text={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil impedit natus nostrum? Velit accusantium id quos, nihil vel quod.Quas, magni mollitia, aspernatur consequatur accusamus vero eum facere exercitationem velit suscipit ipsam placeat libero. '} 
-                    />
-                </div>
-            </InnerLayout>
-        </ResumeStyled>    
-    )
+  const briefcase = <BusinessCenterIcon />;
+  const school = <SchoolIcon />;
+  return (
+    <ResumeStyled>
+      <Title title={"Resume"} span={"resume"} />
+      <InnerLayout>
+        <div className="small-title">
+          <SmallTitle icon={briefcase} title={"Working Experience"} />
+        </div>
+        <div className="resume-content">
+          <ResumeItem
+            year={"2015-2018"}
+            title={"Subject Matter Expert - Non Technical"}
+            subTitle={"Accenture Solutions Pvt Ltd"}
+            text={
+              "I worked as a Subject Matter Expert supporting tech support to help customers with Chromebooks, Google Pixel Phones, Google Play Store & Digital Content. It was a non-technical & hardware tech support role."
+            }
+          />
+          <ResumeItem
+            year={"2018 - Present"}
+            title={"Assistant Manager Operation - Non Technical"}
+            subTitle={"Concentrix Daksh Services Pvt Ltd"}
+            text={`I'm currently working as an Assistant Manager Operation. I handle a team of 30 direct reportees as a team leader. My team is responsible for content moderation. It is a non-technical & people management role.`}
+          />
+          <ResumeItem
+            year={"2021 - Present"}
+            title={"Full Stack Developer"}
+            subTitle={"Freelancer & Learner"}
+            text={
+              "Starting 2021, I enrolled to Fullstack development bootcamp by The Hacking School. After completion, I have been working on several projects & freelancing to improve my portfolio for a full time web developer."
+            }
+          />
+        </div>
+        <div className="small-title u-small-title-margin">
+          <SmallTitle icon={school} title={"Educational Qualifications"} />
+        </div>
+        <div className="resume-content ">
+          <ResumeItem
+            year={"2009 - 2013"}
+            title={"E&E Engineering - Bachelors"}
+            subTitle={"Biju Patnaik University of Technology, Odisha, India"}
+            text={
+              "I completed my bachelors in E&E Engineering in 2013 from BPUT, Odisha, India."
+            }
+          />
+          <ResumeItem
+            year={"2007 - 2009"}
+            title={"College - 10th +2"}
+            subTitle={"Kendriya Vidyalaya, Angul, Odisha, India"}
+            text={
+              "I completed my college (10th +2) in 2009 from Kendriya Vidyalaya, Angul, Odisha, India."
+            }
+          />
+          <ResumeItem
+            year={"2006 - 2007"}
+            title={"High School Graduation"}
+            subTitle={"Maharishi Vidya Mandir, Angul, Odisha, India"}
+            text={
+              "I completed my high school graduation in 2007 from Maharishi Vidya Mandir, Angul, Odisha, India."
+            }
+          />
+        </div>
+      </InnerLayout>
+    </ResumeStyled>
+  );
 }
 
 const ResumeStyled = styled.section`
-    .small-title{
-        padding-bottom: 3rem;
-    }
-    .u-small-title-margin{
-        margin-top: 4rem;
-    }
+  .small-title {
+    padding-bottom: 3rem;
+  }
+  .u-small-title-margin {
+    margin-top: 4rem;
+  }
 
-    .resume-content{
-        border-left: 2px solid var(--border-color);
-    }
+  .resume-content {
+    border-left: 2px solid var(--border-color);
+  }
 `;
-export default Resume
+export default Resume;
