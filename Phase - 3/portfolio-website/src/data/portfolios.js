c98f83f1d9bbed59aea7a1e68f8636f64ce848e2
rksdev <mailto.dev.rakesh@gmail.com>
2022-09-02 20:09:29 +0530

    minor changes

--- Phase - 3/portfolio-website/src/data/portfolios.js
+++ Phase - 3/portfolio-website/src/data/portfolios.js
@@ -1,73 +1,113 @@
-import css1 from '../img/portImages/cv_css.png';
-import css2 from '../img/portImages/ms.png';
-import react1 from '../img/portImages/react_styled.png';
-import img2 from '../img/portImages/maya-4.jpg';
-import img3 from '../img/portImages/maya-3.jpg';
+import Supportdesk from "../img/portImages/Supportdesk.png";
+import GridFS from "../img/portImages/GridFS.png";
+import NodeJS from "../img/portImages/NodeJS.png";
+import Portfolio from "../img/portImages/Portfolio.png";
+import WingdinG from "../img/portImages/WingdinG.png";
+import SocialMediaApp from "../img/portImages/SocialMediaApp.png";
+import GithubFinder from "../img/portImages/GithubFinder.png";
+import FeedbackApp from "../img/portImages/FeedbackApp.png";
+import HouseMarketPlace from "../img/portImages/HouseMarketPlace.png";
 
 const portfolios = [
-    {
-        id: 1,
-        category: 'CSS',
-        image: css1,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'Responsive Resume',
-        text: 'Created with only HTML and CSS.'
-    },
-    {
-        id: 2,
-        category: 'Python',
-        image: img3,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
-    },
-    {
-        id: 3,
-        category: 'Javascript',
-        image: img3,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
-    },
-    {
-        id: 4,
-        category: 'Animation',
-        image: img2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Use Blender',
-        text: 'Free Animation Software'
-    },
-    {
-        id: 5,
-        category: 'Animation',
-        image: img2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Use Blender',
-        text: 'Free Animation Software'
-    },
-    {
-        id: 6,
-        category: 'React JS',
-        image: react1,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'Responsive Portfolio Website',
-        text: 'Created using different technologies such as Material UI, Styled-Components and more...'
-    },
-    {
-        id: 7,
-        category: 'CSS',
-        image: css2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'Microsoft Website Clone',
-        text: 'Created using HTML and CSS'
-    }
-]
+  {
+    id: 1,
+    category: "Fullstack",
+    image: Supportdesk,
+    link1:
+      "https://github.com/rksdevs/Rakesh-THS-Cohort-F-2021/tree/main/Phase%20-%203/support-desk",
+    link2: "https://www.google.com",
+    title: "Support Desk",
+    text: "A fullstack application where one can create ticket to get help on issues with their devices. This is built using MERN Stack & Redux Toolkit",
+  },
+  {
+    id: 2,
+    category: "React JS",
+    image: GithubFinder,
+    link1:
+      "https://github.com/rksdevs/Rakesh-THS-Cohort-F-2021/tree/main/Phase%20-%202/React-Github-Finder-App",
+    link2: "https://www.google.com",
+    title: "Github Profile Finder",
+    text: "A React JS web application to find Github profiles. It is developed using React JS & github API",
+  },
+  {
+    id: 3,
+    category: "Backend & NodeJs",
+    image: NodeJS,
+    link1:
+      "https://github.com/rksdevs/Rakesh-THS-Cohort-F-2021/tree/main/Phase%20-%203/sneakerfreaker",
+    link2: "https://www.google.com",
+    title: "A backend API for an e-commerce website",
+    text: "A backend API for an e-commerce website. This is built using NodeJs, Express & MongoDB. This is a fully functional API featuring complex schemas for Products, Users etc",
+  },
+  {
+    id: 4,
+    category: "Fullstack",
+    image: HouseMarketPlace,
+    link1:
+      "https://github.com/rksdevs/Rakesh-THS-Cohort-F-2021/tree/main/Phase%20-%202/house-marketplace",
+    link2: "https://www.google.com",
+    title: "House Market Place",
+    text: "A fullstack application where one list their properties for sale or check the available offers to buy. This is built using React JS (frontend) & Firebase (backend)",
+  },
+  {
+    id: 5,
+    category: "Fullstack",
+    image: SocialMediaApp,
+    link1:
+      "https://github.com/rksdevs/Rakesh-THS-Cohort-F-2021/tree/main/Phase%20-%202/house-marketplace",
+    link2: "https://www.google.com",
+    title: "Rakeshsocial-Social Media App",
+    text: "A fullstack application for a social media website. This is built using MERN stack. Although this is fully functional, I'm still adding more features",
+  },
+  {
+    id: 6,
+    category: "React JS",
+    image: FeedbackApp,
+    link1:
+      "https://github.com/rksdevs/Rakesh-THS-Cohort-F-2021/tree/main/Phase%20-%202/React-Feedback-App",
+    link2: "https://www.google.com",
+    title: "React Feedback App",
+    text: "This is a feedback application created using React JS. This is one of my first projects where I used React JS and saved the feedback data in a JSON file to give it a fullstack feature",
+  },
+  {
+    id: 7,
+    category: "Fullstack",
+    image: GridFS,
+    link1:
+      "https://github.com/rksdevs/Rakesh-THS-Cohort-F-2021/tree/main/Phase%20-%203/gridFs",
+    link2: "https://www.google.com",
+    title: "Grid FS Project",
+    text: "A fullstack application for Grid File System. This is one of my very first full stack projects developed using MERN stack, to save files in a database. The frontend uses React MUI & backend is powered by NodeJS, Express & MongoDB",
+  },
+  {
+    id: 8,
+    category: "Fullstack",
+    image: WingdinG,
+    link1: "https://github.com/rksdevs/wingding-v1",
+    link2: "https://www.google.com",
+    title: "Website for Service Based Company",
+    text: "This is one of my first freelancing project, where I've designed a website for a company that offers various services around Event Management. This website was designed using React JS, MUI for frontend, and Firebase for backend",
+  },
+  {
+    id: 9,
+    category: "Backend & NodeJs",
+    image: NodeJS,
+    link1:
+      "https://github.com/rksdevs/Rakesh-THS-Cohort-F-2021/tree/main/Phase%20-%203/learners-inn/Backend",
+    link2: "https://www.google.com",
+    title: "Learner Inn - Backend",
+    text: "This is my personal project which is a social media app for learners. This is a work in progress, I'm currently working on the backend & APIs, and I will gradually move to the frontend. Technologies I've used on this project so far are NodeJs, ExpressJS & MongoDB",
+  },
+  {
+    id: 10,
+    category: "React JS",
+    image: Portfolio,
+    link1:
+      "https://github.com/rksdevs/Rakesh-THS-Cohort-F-2021/tree/main/Phase%20-%203/portfolio-website",
+    link2: "https://www.google.com",
+    title: "Portfolio Website",
+    text: "This is my portfolio website which you're currently viewing. It is made using ReactJS & Styled Components",
+  },
+];
 
 export default portfolios;
