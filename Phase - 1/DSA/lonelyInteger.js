f7459aa99aa17e282455e3a7cee7f832b235f3a0
rksdev <mailto.dev.rakesh@gmail.com>
2022-10-09 13:10:11 +0530

    lonelyInteger - hackerRank

--- Phase - 1/DSA/lonelyInteger.js
+++ Phase - 1/DSA/lonelyInteger.js
@@ -3,10 +3,18 @@ function lonelyinteger(a) {
 
   let dictionary = {};
   for (let item of a) {
-    dictionary.item ? ++dictionary[item] : (dictionary[item] = 1);
+    // dictionary.item = 1 || ++ dictionary[item];
+    if (dictionary[item]) {
+      dictionary[item] = ++dictionary[item];
+    } else {
+      dictionary[item] = 1;
+    }
   }
 
-  dictionary.forEach((item) => {
+  for (let item in dictionary) {
     if (dictionary[item] === 1) return item;
-  });
+  }
+  //   console.log(dictionary);
 }
+
+console.log(lonelyinteger([1, 2, 3, 4, 3, 2, 1]));
